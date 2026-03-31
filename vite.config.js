769313e482132093d52c48e5a5cb12ae77cd874d
yctang-kydon <yingchun.tang@kydongrp.com>
2026-03-31 17:41:34 +0800

    Move files into public folder to enable rendering on Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig ({
-    root: '.', 
+    root: '.',
+    base: '/angularjs-twitter-rubric/', 
 
     build: {
         outDir: 'dist',
